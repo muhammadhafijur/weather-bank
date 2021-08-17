95dbb7220b1061029f7f7c2bda5de96a3efc0f76
MuhammadHafijur <hafijur.salah@gmail.com>
2021-08-17 22:44:38 +0600

    added event handler

--- js/bank.js
+++ js/bank.js
@@ -1,3 +1,13 @@
 document.getElementById('login-submit').addEventListener('click', function(){
-    console.log('button-clicked')
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+    if(userEmail == 'demo@gmail.com' && userPassword == 1234) {
+        window.location.href = 'banking.html'
+    } else {
+        document.write('wrong info')
+    }
 })

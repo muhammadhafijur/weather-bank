e8fd088761f705d1f144a936b385454d474dc72a
MuhammadHafijur <hafijur.salah@gmail.com>
2021-08-18 18:29:06 +0600

    added banking.html

--- js/bank.js
+++ js/bank.js
@@ -1,13 +1,15 @@
 document.getElementById('login-submit').addEventListener('click', function(){
+    // get user email
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
     // get user password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
-    
+    // check email and password
     if(userEmail == 'demo@gmail.com' && userPassword == 1234) {
         window.location.href = 'banking.html'
-    } else {
-        document.write('Wrong password');
+    }
+    else {
+        document.write('please use valid username and password');
     }
 })
